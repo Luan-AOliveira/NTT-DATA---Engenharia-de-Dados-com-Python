116bd656b30e68bb4138c3980dcf3548f381fb15
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2024-08-22 01:51:05 -0300

    tipos-de-dados

---
+++ tipos_de_dados.py
@@ -0,0 +1,19 @@
+# Número inteiro int
+print(11 + 10 + 1000)
+
+# Número quebrado flot
+print(1.5 + 1 + 0.5)
+
+# Booleano verdadeiro/falso bool
+print(True)
+print(False)
+
+# String str
+print("Python")
+
+
+int()
+float()
+bool()
+str()
+

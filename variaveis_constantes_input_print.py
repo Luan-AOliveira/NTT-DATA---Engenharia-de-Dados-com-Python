89c5a70e7db5b26b07c213f45c288037c4901b97
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2024-08-22 02:11:44 -0300

    variaveis_constantes_input_print

--- variaveis_constantes_input_print.py
+++ variaveis_constantes_input_print.py
@@ -7,3 +7,8 @@ print(f"{nome}, {idade}")
 
 brazilian_states = ["SP", "RJ", "SC", "RS"]
 print(brazilian_states)
+
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+
+print(f"O nome da pessoa é {nome} e a idade é {idade}")

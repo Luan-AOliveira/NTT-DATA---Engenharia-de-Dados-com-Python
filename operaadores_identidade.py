d47ca40c6e67aed95f501574cdf6fff2eca63914
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2024-08-23 01:29:59 -0300

    operadores_identidade

---
+++ operaadores_identidade.py
@@ -0,0 +1,5 @@
+saldo = 1000
+limite = 1000  # se ambos a comparação for o mesmo valor da verdadeiro se não foi o mesmo valor da false
+
+print(saldo is limite) # IS é para saber se eles ocupam a mesma área de memória
+print(saldo is not limite) # IS NOT é falando que eles não são da mesma área de memoria

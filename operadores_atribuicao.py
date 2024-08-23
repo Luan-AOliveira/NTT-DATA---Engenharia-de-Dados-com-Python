9956d13c5cb8e3e590141edad23d587a989ecdbe
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2024-08-23 01:01:06 -0300

    operadores_atribuicao

---
+++ operadores_atribuicao.py
@@ -0,0 +1,30 @@
+saldo = 200
+print(saldo)
+
+
+saldo += 10
+print(saldo)
+
+
+saldo -= 5
+print(saldo)
+
+
+saldo //= 2 # só retorna número inteiro se antes disso virar float esta função não ira funcionar 
+print(saldo)
+
+
+saldo /= 2
+print(saldo)
+
+
+saldo *= 10
+print(saldo)
+
+
+saldo %= 4 # função módulo serve para retornar  resto da divisão
+print(saldo)
+
+
+saldo **= 2
+print(saldo)

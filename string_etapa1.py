05277304509ef53f5cbf03b18633410cce1081f3
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2024-08-28 09:33:00 -0300

    f-string

--- string_etapa1.py
+++ string_etapa1.py
@@ -17,3 +17,10 @@ menu = "Python"
 print("####" + menu + "####")
 print(menu.center(14, "#"))
 print("-".join(menu))
+
+
+
+#f-string pra aparecer só o tanto de casas que eu quero q seja imprimido
+PI = 3.14159
+
+print(f"O valor de PI: {PI:.2f}") # vai aparecer 3.14 somente

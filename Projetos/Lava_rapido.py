bdef9e3a812e742d413a1b92795afb7e059bc81f
Luan Alves Oliveira <144074045+Luan-AOliveira@users.noreply.github.com>
2024-09-05 10:40:05 -0300

    Projeto Lava-Rapido (data e hora)

--- Projetos/Lava_rapido.py
+++ Projetos/Lava_rapido.py
@@ -5,20 +5,17 @@ from datetime import timedelta, datetime, time, date
 tempo_pequeno = 30
 tempo_medio = 45
 tempo_grande = 60
-data_atual = datetime.now() #datetime e now vão pegar a data e hora e região do mundo atual do pc 
+data_atual = datetime.now()  
 
 
 infome_tamanho = input("Informe o tamanho do seu carro com P, M ou G: ")
 if infome_tamanho in ["P", "M", "G"]:
-    # Calcula a data estimada de conclusão
     if infome_tamanho == "P":
         data_estimada = data_atual + timedelta(minutes=tempo_pequeno)
     elif infome_tamanho == "M":
         data_estimada = data_atual + timedelta(minutes=tempo_medio)
     else:
         data_estimada = data_atual + timedelta(minutes=tempo_grande)
-
-    # Imprime o resultado
     print(f"O carro chegou às: {data_atual:%d/%m/%Y às %H:%M} e ficará pronto às: {data_estimada:%d/%m/%Y às %H:%M}")
 else:
     print("Tamanho de carro inválido. Por favor, informe P, M ou G.")
